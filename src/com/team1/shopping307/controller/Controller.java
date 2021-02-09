a2acb9cbeed49c8d26e7ebc259d3f5a9a3c9ce06
Skswlatmddlek1 <ccangse18@naver.com>
2021-02-09 15:14:21 +0900

    로그인 화면을 jsp 만들었습니다.

--- src/com/team1/shopping307/controller/Controller.java
+++ src/com/team1/shopping307/controller/Controller.java
@@ -20,7 +20,7 @@ public class Controller extends HttpServlet {
    public Controller() {
       super();
    }
-
+   
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       // response.getWriter().append("Served at: ").append(request.getContextPath());
       request.setCharacterEncoding("UTF-8");

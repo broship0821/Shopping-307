03234cfc75688623cbc7efc75e8360c527cdfcba
Skswlatmddlek1 <ccangse18@naver.com>
2021-02-10 14:30:40 +0900

    userDAO userVO만들었습니다.

--- src/com/team1/shopping307/service/TelService.java
+++ src/com/team1/shopping307/service/TelService.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.team1.shopping307.VO.Member1VO;
 
 public interface TelService {
    public void getAllInfo(HttpServletRequest request, HttpServletResponse response) throws Exception;
@@ -12,4 +15,5 @@ public interface TelService {
    public void insertInfo(HttpServletRequest request, HttpServletResponse response) throws Exception;
    public void getOneInfo(HttpServletRequest request, HttpServletResponse response) throws Exception;
    public void updateInfo(HttpServletRequest request, HttpServletResponse response) throws Exception;
+   
 }

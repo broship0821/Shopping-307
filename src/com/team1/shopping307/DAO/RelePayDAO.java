bef2d8229f77bac2afe576073688685dc00d544e
peter <gudqo1995@naver.com>
2021-02-19 11:56:26 +0900

    RelePayDAO수정-Libs에 있는 commit(),rollback()으로 변경/release_id전부String으로 변경

--- src/com/team1/shopping307/DAO/RelePayDAO.java
+++ src/com/team1/shopping307/DAO/RelePayDAO.java
@@ -22,28 +22,40 @@ public class RelePayDAO {
 	// 결제시 release_history, pay_info 테이블 insert 작업을 하나의 메소드로 하나의 트랜잭션으로 처리
 	public int insertRelePay(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		// 현재 로그인 사용자 정보 가져오기
+		String userID = LoginManager.getUserInfo(request).getUserId();
+		
 		// 결제페이지에서 보낸 데이터
 		String[] ProdIDs = request.getParameterValues("ProductID"); // 상품 번호(상품이 여러개니 배열로 받아야됨)
 		String[] cnt = request.getParameterValues("cnt"); // 상품 개수(상품이 여러개니 배열로 받아야됨)
-		String name = request.getParameter("name"); // 구매자 이름
-		String zip = request.getParameter("zip"); // 구매자 우편번호
-		String address = request.getParameter("address"); // 구매자 주소
-		String tel = request.getParameter("tel"); // 구매자 전화번호
-		String cashOrCard = request.getParameter("cashOrCard"); // 'C':카드, 'S':현금, 'B':무통장입금
-		String cardNo = request.getParameter("cardNO"); // 카드번호
+		// pay_info에 insert
+		PayVO payVO = new PayVO();
+		// 판매자 일단 "undefined" 처리
+		payVO.setSellerName("undefined");
+		payVO.setSellerPhone("0");
+		payVO.setSellerZip("0");
+		payVO.setSellerAddress("undefined");
+		// 구매한 유저는 로그인된 유저
+		payVO.setBuyerId(userID);
+		payVO.setBuyerName(request.getParameter("name"));
+		payVO.setBuyerPhone(request.getParameter("tel"));
+		payVO.setBuyerZip(request.getParameter("zip"));
+		payVO.setBuyerAddress(request.getParameter("address"));
+		payVO.setCashOrCard(request.getParameter("cashOrCard"));
+		payVO.setCardNo(request.getParameter("cardNO"));
+		payVO.setStatus(STATUS);
+					
 		// pay_info에 필요한 items_info, amount
 		StringBuffer itemsInfo = new StringBuffer();
 		int amount = 0;
-
-		// 현재 로그인 사용자 정보 가져오기
-		String userID = LoginManager.getUserInfo(request).getUserId();
-
+		
 		// db 작업
 		Connection conn = null;
-		long result1 = 0;// release_history insert 결과 성공시 release_id 반환
+		String result1 = "";// release_history insert 결과 성공시 release_id 반환
 		long result2 = 0;// pay_info insert 결과 성공시 pay_id 반환
 		try {
 			conn = Libs.connectWithManualCommit();
+			Libs.setAutoCommit(conn, false);
 
 			for (int i = 0; i < ProdIDs.length; i++) {
 				// 먼저 select로 필요한 Product 정보 추출
@@ -61,43 +73,23 @@ public class RelePayDAO {
 				itemsInfo.append(size + ", ");
 				amount += vo.getPrice() * size;// 물품금액*갯수
 			}
-			// pay_info에 insert
-			PayVO payVO = new PayVO();
-			// 판매자 일단 "undefined" 처리
-			payVO.setSellerName("undefined");
-			payVO.setSellerPhone("0");
-			payVO.setSellerZip("0");
-			payVO.setSellerAddress("undefined");
-			// 구매한 유저는 로그인된 유저
-			payVO.setBuyerId(userID);
-			payVO.setBuyerName(name);
-			payVO.setBuyerPhone(tel);
-			payVO.setBuyerZip(zip);
-			payVO.setBuyerAddress(address);
 			payVO.setItemsInfo(itemsInfo.toString());
 			payVO.setAmount(amount);
-			payVO.setCashOrCard(cashOrCard);
-			payVO.setCardNo(cardNo);
-			payVO.setStatus(STATUS);
-
+			
 			result2 = new PayDAO().insert(conn, payVO, true);
 			//에러시 롤백
-			if(result2==0) conn.rollback();
+			if(result2==0) Libs.rollback(conn);
 			
 			//마지막에 커밋
-			conn.commit();
-			conn.setAutoCommit(true);
+			Libs.commit(conn);
+			Libs.setAutoCommit(conn, true);
 
 		} catch (Exception e) {
+			Libs.rollback(conn);
 			e.printStackTrace();
-			if (conn != null)
-				try {
-					conn.rollback();
-				} catch (Exception e2) {
-				}
 		}
 
-		return (result1 != 0 && result2 != 0) ? 1 : 0;
+		return (!("".equals(result1)) && result2 != 0) ? 1 : 0;
 	}
 
 	// product 테이블에서 select로 필요한 정보 추출
@@ -135,9 +127,9 @@ public class RelePayDAO {
 	}
 
 	// release_history에 값 저장하기
-	public static long insert(Connection conn, ReleHistVO vo) {
+	public static String insert(Connection conn, ReleHistVO vo) {
 		System.out.println(className + ".insert()");
-		long result = 0;
+		String result = "";
 		PreparedStatement ps = null;
 
 		if (conn != null) {
@@ -152,9 +144,9 @@ public class RelePayDAO {
 			try {
 				ps = conn.prepareStatement(sql);
 				int idx = 0;
-				vo.setReleId(Long.valueOf(Libs.getNewId(conn, "", "seq_rele", "release_id")));
+				vo.setReleId((Libs.getNewId(conn, "", "seq_rele", "release_id")));
 				
-				ps.setLong(++idx, vo.getReleId());
+				ps.setString(++idx, vo.getReleId());
 				ps.setString(++idx, vo.getUserId());
 				ps.setString(++idx, vo.getProdId());
 				ps.setString(++idx, vo.getProdName());
